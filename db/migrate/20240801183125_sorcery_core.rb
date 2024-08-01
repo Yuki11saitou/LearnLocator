7053a180b928f28b6fdf820b53b2a41be74f543e
Yuki11saitou <3110yuki0725@gmail.com>
2024-08-02 04:15:44 +0900

    feat: #21 usersテーブル作成

---
+++ db/migrate/20240801183125_sorcery_core.rb
@@ -0,0 +1,14 @@
+class SorceryCore < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name,             null: false
+      t.string :email,            null: false, index: { unique: true }
+      t.string :avatar
+      t.string :crypted_password
+      t.string :salt
+      t.integer :role, default: 0
+
+      t.timestamps                null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_31_122027) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_183125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_31_122027) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "avatar"
+    t.string "crypted_password"
+    t.string "salt"
+    t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

c84e216fac987da4033e7fd6dc61c37c3201cf31
Yuki11saitou <3110yuki0725@gmail.com>
2024-09-01 10:15:15 +0900

    feat: #132 gem i18nの導入

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Myapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Railsアプリケーションのデフォルトの言語設定を日本語に設定
+    config.i18n.default_locale = :ja
   end
 end

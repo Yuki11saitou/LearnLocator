dd0b2a44571538d37c72728dd0537e9a255a6309
Yuki11saitou <3110yuki0725@gmail.com>
2024-09-03 01:10:03 +0900

    feat: #183 logout時のフラッシュメッセージ表示

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -16,6 +16,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other, notice: 'ログアウトしました'
   end
 end

42279133039735790474ba338d2e4b81ba39056e
Yuki11saitou <3110yuki0725@gmail.com>
2024-09-13 17:48:31 +0900

    feat: #72 ページネーションの表示数を設定

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Kaminari.configure do |config|
-  config.default_per_page = 5
+  config.default_per_page = 5 # 1ページあたりの表示件数
   # config.max_per_page = nil
-  # config.window = 4
-  # config.outer_window = 0
+  config.window = 2 # 現在のページの前後に表示するページ数
+  # config.outer_window = 0 # 最初と最後に表示するページ数
   # config.left = 0
   # config.right = 0
   # config.page_method_name = :page

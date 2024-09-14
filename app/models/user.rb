96950499594e9a3506743d03e05ce8ecafa7585b
Yuki11saitou <3110yuki0725@gmail.com>
2024-09-14 21:16:59 +0900

    refactor: #139 rubocopの指摘に伴いuserモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  enum role: { general: 0, admin: 1 }
+  enum :role, { general: 0, admin: 1 }
 
   validates :password, length: { minimum: 4 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }

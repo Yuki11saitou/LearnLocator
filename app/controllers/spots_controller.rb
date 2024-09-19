4311bb007c1788796551ba133c625c0eff2ec806
Yuki11saitou <3110yuki0725@gmail.com>
2024-09-20 00:07:40 +0900

    refactor: #42 spot_controllerでN+1問題について考慮してないアクションがあったため修正

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,11 +2,13 @@ class SpotsController < ApplicationController
   skip_before_action :require_login
 
   def map
-    @spots = Spot.all
+    @spots = Spot.includes(:category).all
   end
 
   def index
-    @spots = Spot.all.page(params[:page])
+    # @q = Spot.ransack(params[:q])
+    # @spots = @q.result(distinct: true).includes(:category).order(created_at: :desc).page(params[:page])
+    @spots = Spot.includes(:category).all.page(params[:page])
   end
 
   def show

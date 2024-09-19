fa020482a76a65c5f93e9ad87b9f7c6035c29f7f
Yuki11saitou <3110yuki0725@gmail.com>
2024-09-19 23:52:01 +0900

    feat: #42 ransack設定をモデルに追記

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,4 +3,14 @@ class Spot < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
   validates :name, :latitude, :longitude, :place_id, presence: true, uniqueness: true
+
+  # ransackの許可リスト(Spotモデルの検索条件に使用するカラム名を設定)
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "category_id", "created_at", "id", "id_value", "latitude", "longitude", "name", "opening_hours", "phone_number", "photo_reference", "place_id", "postal_code", "rating", "updated_at", "web_site"]
+  end
+
+  # ransackの許可リスト(Spotモデルの関連モデルの検索条件に使用するカラム名を設定)
+  def self.ransackable_associations(auth_object = nil)
+    ["address"]
+  end
 end

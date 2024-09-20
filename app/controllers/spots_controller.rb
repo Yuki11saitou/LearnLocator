90e0e500eb709a15e5f36dd7af323c3ff527aa25
Yuki11saitou <3110yuki0725@gmail.com>
2024-09-20 20:51:22 +0900

    feat: #42 セレクトタグのカテゴリを動的に取得できるよう、コントローラーに設定追記

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -8,6 +8,8 @@ class SpotsController < ApplicationController
   def index
     @q = Spot.ransack(params[:q])
     @spots = @q.result(distinct: true).includes(:category).order(created_at: :desc).page(params[:page])
+    # ソート機能のため、カテゴリを取得(現状、カテゴリID:1(自習室), 2(コワーキング)のみ)
+    @categories = Category.where(id: [1, 2])
   end
 
   def show

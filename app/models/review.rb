3f8ffa31ccd958a7f7fdc686e577e74ec2990617
Yuki11saitou <3110yuki0725@gmail.com>
2024-10-07 02:51:41 +0900

    text: #62 不要なコメント削除

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,14 +3,5 @@ class Review < ApplicationRecord
   belongs_to :spot
   has_many :likes, dependent: :destroy
 
-  # todo : 後で精査
-  # validates :body, presence: true, length: { maximum: 65_535 }
   validates :body, presence: true, length: { maximum: 300 }
-
-
-  # todo: bookmark機能の実装時に有効化
-
-  # def bookmark_by?(user)
-  #   bookmarks.exists?(user_id: user.id)
-  # end
 end

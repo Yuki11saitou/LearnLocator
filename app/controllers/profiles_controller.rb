e126c6a34523fefd0b2931747c34b9a9e06855ae
Yuki11saitou <3110yuki0725@gmail.com>
2024-10-11 21:18:49 +0900

    feat: #35 profilesコントローラーの作成と設定

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,33 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
+  def show
+    @total_my_reviews = @user.reviews.count
+    @total_my_likes = @user.like_reviews.count
+
+    # Spotに紐づくCategoryも含める
+    @my_reviews = @user.reviews.includes(spot: :category).order(created_at: :desc).page(params[:page])
+    @my_likes = @user.like_reviews.includes(spot: :category).order(created_at: :desc).page(params[:page])
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, notice: t('notices.profile_update_success')
+    else
+      flash.now[:alert] = t('alerts.profile_update_failure')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar, :avatar_cache)
+  end
+end

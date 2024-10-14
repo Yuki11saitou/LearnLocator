85e4af6b1e894a8587a7892b3d932dde7392e6e0
Yuki11saitou <3110yuki0725@gmail.com>
2024-10-14 22:43:17 +0900

    refactor: #183 Spotコントローラーのindexアクションをrubocopに従い修正

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -9,26 +9,26 @@ class SpotsController < ApplicationController
     # 口コミ数でのソートを考慮し、LEFT JOIN でレビュー数を取得しておく
     @q = Spot.left_joins(:reviews).select('spots.*, COUNT(reviews.id) AS reviews_count').group('spots.id').ransack(params[:q])
 
-    if params.dig(:q, :s)&.include?('reviews_count')
-      # 口コミ数で並べ替える場合
-      @spots = @q.result(distinct: true)
-                .includes(:category, :reviews)
-                .order("reviews_count #{params.dig(:q, :s).split.last.upcase}") # ここで reviews_count を明示的に並べ替え
-                .page(params[:page]).per(5)
-    elsif params.dig(:q, :s)&.include?('rating')
-      # 評価順で並べ替える場合
-      @spots = @q.result(distinct: true)
-                .includes(:category, :reviews)
-                .where.not(rating: nil)
-                .order(created_at: :desc)
-                .page(params[:page]).per(5)
-    else
-      # 並べ替えリンクを使用しない場合
-      @spots = @q.result(distinct: true)
-                .includes(:category, :reviews)
-                .order(created_at: :desc)
-                .page(params[:page]).per(5)
-    end
+    @spots = if params.dig(:q, :s)&.include?('reviews_count')
+               # 口コミ数で並べ替える場合
+               @q.result(distinct: true)
+                 .includes(:category, :reviews)
+                 .order("reviews_count #{params.dig(:q, :s).split.last.upcase}") # ここで reviews_count を明示的に並べ替え
+                 .page(params[:page]).per(5)
+             elsif params.dig(:q, :s)&.include?('rating')
+               # 評価順で並べ替える場合
+               @q.result(distinct: true)
+                 .includes(:category, :reviews)
+                 .where.not(rating: nil)
+                 .order(created_at: :desc)
+                 .page(params[:page]).per(5)
+             else
+               # 並べ替えリンクを使用しない場合
+               @q.result(distinct: true)
+                 .includes(:category, :reviews)
+                 .order(created_at: :desc)
+                 .page(params[:page]).per(5)
+             end
 
     # ソート機能のため、カテゴリを取得(自習室とコワーキングのみ)
     @categories = Category.where(id: [1, 2])

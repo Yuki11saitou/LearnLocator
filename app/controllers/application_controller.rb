f5a380d304d50764db10229bd6ce95782798ac1e
Yuki11saitou <3110yuki0725@gmail.com>
2024-10-20 19:19:24 +0900

    feat: #266 存在しないURLに対する対策

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  # 存在しないレビューを参照しようとした場合は、トップページにリダイレクト
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
   def not_authenticated
     redirect_to login_path, alert: t('alerts.require_login')
+  end
+
+  def record_not_found
+    redirect_to root_path, alert: t('alerts.not_found')
   end
 end

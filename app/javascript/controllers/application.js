4cb541a5132c886330ee6b9f50e4d67276177812
Yuki11saitou <3110yuki0725@gmail.com>
2024-10-21 00:35:04 +0900

    chore: #155 stimulus-autocompleteの設定を記述

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,12 @@
 import { Application } from "@hotwired/stimulus"
 
+// #コンポーネントを読み込むための記述
+import { Autocomplete } from 'stimulus-autocomplete'
+
 const application = Application.start()
+
+// コンポーネントにあるAutocompleteコントローラを使えるようにするための記述
+application.register('autocomplete', Autocomplete)
 
 // Configure Stimulus development experience
 application.debug = false

9864d75bab2587747337c9c9fbf2feb1fae02db5
Yuki11saitou <3110yuki0725@gmail.com>
2024-10-25 16:50:34 +0900

    feat: #128 #129 コントローラー設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
 
   # todo: differenceについては、後ほど精査
-  skip_before_action :require_login, only: %i[top difference]
+  skip_before_action :require_login, only: %i[top difference terms_of_use privacy_policy]
 
   def top; end
 
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
   # todo: differenceについては、後ほど精査
   def difference; end
 
+  def terms_of_use; end
+
+  def privacy_policy; end
 end

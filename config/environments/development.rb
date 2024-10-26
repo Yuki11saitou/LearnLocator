07fea9bb49bd1eeb5af14da65025d8f154ecec4a
Yuki11saitou <3110yuki0725@gmail.com>
2024-10-26 22:46:58 +0900

    feat: #309 リクエスト制限を有効にするため、開発環境でのキャッシュ設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,9 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    # 開発環境でのキャッシュ管理（デフォルトはnull_store）｜これはRack Attackを用いたリクエスト制限のために設定
+    # config.cache_store = :null_store
+    config.cache_store = :memory_store
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).

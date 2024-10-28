caa7caa9697c62af9cd686257b2120efa725d977
Yuki11saitou <3110yuki0725@gmail.com>
2024-10-29 02:35:20 +0900

    feat: #13 tailwind側のclassによるフォントの適用設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,31 @@ module.exports = {
     // 使用テーマの設定
     themes: ["cmyk"],
     // ダークモードの無効化
-    darkTheme: false,
+    darkTheme: false
+  },
+
+  theme: {
+    extend: {
+      fontFamily: {
+        'body-jp': ['Noto Sans JP', 'serif'],
+        'body-en': ['Noto Sans', 'serif']
+      },
+      // // 画像、テキストのアニメーション追加
+      // animation: {
+      //   "fade-in-fwd": "fade-in-fwd 1.9s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both"
+      // },
+      // keyframes: {
+      //   "fade-in-fwd": {
+      //     "0%": {
+      //         transform: "translateZ(-80px)",
+      //         opacity: "0"
+      //     },
+      //     to: {
+      //         transform: "translateZ(0)",
+      //         opacity: "1"
+      //     }
+      //   }
+      // }
+    }
   }
 }

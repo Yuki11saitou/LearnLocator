d5343f02e90f8f8d71fa500414d6e082956286d7
Yuki11saitou <3110yuki0725@gmail.com>
2024-11-12 09:47:42 +0900

    test: #365 factorybotの設定の記述修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.unique.name }
-    email {Faker::Internet.email}
-    password {Faker::Internet.password(min_length: 4)}
-    password_confirmation {password}
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 4) }
+    password_confirmation { password }
   end
 end
